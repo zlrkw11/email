9e5fc747943cab1ad9288e1af7bc9d730d0b4672
ZL <ray040406@gmail.com>
2024-06-23 11:23:56 +1200

    import

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
+import EmailSender from "./components/EmailSender";
 function App() {
-  const sendEmail = (e) => {
-    e.preventDefault();
-  };
-  return (
-    <div>
-      <form onSubmit={sendEmail}>
-        <h1>Email Sender</h1>
-        <button type="submit">send</button>
-      </form>
-    </div>
-  );
+  return <EmailSender />;
 }
 
 export default App;
