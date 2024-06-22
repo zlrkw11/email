4000de486b1162d018bbdb38586b720110cd999e
ZL <ray040406@gmail.com>
2024-06-23 11:19:22 +1200

    set up form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import { useState } from "react";
-
 function App() {
-  return <></>;
+  const sendEmail = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <h1></h1>
+      <form onSubmit={sendEmail}>
+        <button type="submit">send</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
